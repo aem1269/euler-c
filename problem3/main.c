a41ce3b1269e213859d555f3eed30552652993bd
Adam McCarthy <aem1269@rit.edu>
2014-09-23 18:51:28 -0400

    Start problem 3

---
+++ problem3/main.c
@@ -0,0 +1,21 @@
+/*
+ * problem3/main.c
+ *
+ * https://projecteuler.net/problem=3
+ * What is the largest prime factor of the number 600851475143
+ *
+ * NOTE: This program must be compiled with C99
+ */
+# define NUMBER (600851475143)
+# define NUMBITS = (sizeof long * CHAR_BIT)
+# include <stdio.h>
+# include <stdlib.h>
+# include <inttypes.h>
+
+/*
+ * Prints the largest prime factor of the number and exits successfully
+ */
+int main() {
+	uint64_t number = NUMBER;
+	exit(EXIT_SUCCESS);
+}
