1ad7c24051bcf1196b7a555c55cb8f8edf521294
Adam McCarthy <aem1269@rit.edu>
2014-09-24 02:32:44 -0400

    Makefile and skeleton code for Problem 4

---
+++ problem4/main.c
@@ -0,0 +1,17 @@
+/*
+ * problem4/main.c
+ *
+ * https://projecteuler.net/problem=4
+ * Find the largest palindrome made from the product of two 3-digit numbers
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Finds the largest such palindrome and exits successfully
+ */
+int main() {
+	int palindrome = 0;
+	printf("Largest palindrome found: %d\n", palindrome);
+	exit(EXIT_SUCCESS);
+}
