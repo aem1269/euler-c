3005a0fab5b8f9907dad3dc16217d35629f4feba
Adam McCarthy <aem1269@rit.edu>
2014-09-24 20:43:53 -0400

    Write simple isPalindrome function

--- problem4/main.c
+++ problem4/main.c
@@ -8,6 +8,33 @@
 #include <stdlib.h>
 
 /*
+ * Determines if a given integer is a palindrome
+ *
+ * Parameter: num
+ * 	The potential palindrome
+ * Returns:
+ * 	0 if the number is not a palindrome
+ * 	A non-zero value otherwise.
+ */
+int isPalindrome(int num) {
+	char* s = malloc(sizeof(char) * 7); /*6 digits and a null terminator*/
+	char* front = s;
+	char* back = s + 6;
+	sprintf(s, "%d", num);
+
+	/* move to the last character */
+	while (!(*(--back)));
+	while (*front) {
+		if (*(front++) != *(back--)) {
+			return 0;
+		}
+	}
+	return 1;
+
+
+}
+
+/*
  * Finds the largest such palindrome and exits successfully
  */
 int main() {
