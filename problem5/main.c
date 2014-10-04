874355e819cd214e64a995c958f1bbc17647026c
Adam McCarthy <aem1269@rit.edu>
2014-10-04 19:35:01 -0400

    Create makefile and skeleton code for problem 5

---
+++ problem5/main.c
@@ -0,0 +1,15 @@
+/*
+ * problem5/main.c
+ *
+ * https://projecteuler.net/problem=5
+ * What is the smallest positive number that is evenly divisible by all of the
+ * numbers from 1 to 20?
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int ans = 0;
+	printf("LCM of all numbers 1 to 20 is %d\n", ans);
+	exit(EXIT_SUCCESS);
+}
