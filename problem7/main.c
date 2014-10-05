88f7c62a0583843b994e386786f32d973236746a
Adam McCarthy <aem1269@rit.edu>
2014-10-05 00:22:38 -0400

    skeleton for problem 7

---
+++ problem7/main.c
@@ -0,0 +1,17 @@
+/*
+ * problem7/main.c
+ *
+ * https://projecteuler.net/problem=7
+ * What is the 10 001st prime number?
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#define NUMBER_OF_PRIMES (10001)
+
+/*
+ * Print out the 10 001st prime and then exit
+ */
+int main() {
+	int prime = 0;
+	printf("10,001st prime: %d\n", prime);
+}
